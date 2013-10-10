38e9218df8fa1cbdda8dd5d2af691e70f545b0f9
Ian <ian@meepshop.com>
2013-10-11 00:28:03 +0800

    fb url

--- public/js/product.js
+++ public/js/product.js
@@ -133,7 +133,7 @@
 
 	if($('#comments').length > 0) {
 		function changePluginUrl(value) {
-			var newVal = '<fb:comments href="http://beta.tresor.tw/product/' + value + '/" num_posts="20" width="100%"></fb:comments>';
+			var newVal = '<fb:comments href="http://tresor.tw/product/' + value + '/" num_posts="20" width="100%"></fb:comments>';
 			$('#comments').html(newVal);
 			FB.XFBML.parse($('#comments').get(0),function(){
 				$(".FB_Loader").remove();
